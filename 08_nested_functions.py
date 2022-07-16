db93fa0c0d0d6586470a18fa51fe552d8bcefe49
Robert Vári <mail.robertvari@gmail.com>
2022-07-16 16:49:27 +0200

    update

---
+++ 08_nested_functions.py
@@ -0,0 +1,19 @@
+def calculate_result(a, b):
+    def add_numbers(x, y):
+        return x+y
+
+    def multiply_numbers(x, y):
+        return x*y
+
+    def divide_numbers(x, y):
+        return x/y
+
+    add_result = add_numbers(a, b)
+    multiply_result = multiply_numbers(add_result, b)
+    final_result = divide_numbers(multiply_result, b)
+
+    return final_result
+
+
+result = calculate_result(45, 3.4)
+print(result)
