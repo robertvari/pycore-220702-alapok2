7fcc5b10c20d6aeb17ff3b793f02543cc9c820f6
Robert Vári <mail.robertvari@gmail.com>
2022-07-16 16:28:45 +0200

    update

--- 07_default_parameters.py
+++ 07_default_parameters.py
@@ -1,4 +1,16 @@
 def registration_form(email, password1, password2):
+    if "@" not in email:
+        print("Email is not valid")
+        return  # early return
+
+    if len(password1) < 6:
+        print("Password must contain minimum 6 characters.")
+        return
+
+    if password1 != password2:
+        print("Passwords are not the same")
+        return
+
     print("Thank you for your registration.")
     print(f"We sent an email to {email} with further instructions.")
 
