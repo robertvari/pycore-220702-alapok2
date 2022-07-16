83f47b07bdb69a2704c351293c2d31ecd5d5cf85
Robert Vári <mail.robertvari@gmail.com>
2022-07-16 17:16:00 +0200

    update

--- 09_decorators.py
+++ 09_decorators.py
@@ -1,37 +1,28 @@
 import random, time
+from my_python_library.decorators import timer
 
 
+@timer
 def worker1():
     print("Worker1 started...")
     time.sleep(random.randint(1, 6))
     print("Worker1 finished!")
 
 
+@timer
 def worker2():
     print("Worker2 started...")
     time.sleep(random.randint(1, 6))
     print("Worker2 finished!")
 
 
+@timer
 def worker3():
     print("Worker3 started...")
     time.sleep(random.randint(1, 6))
     print("Worker3 finished!")
 
 
-start = time.time()
 worker1()
-stop = time.time()
-
-print(f"Worker1 processtime: {stop - start}")
-
-start = time.time()
 worker2()
-stop = time.time()
-print(f"Worker2 processtime: {stop - start}")
-
-
-start = time.time()
 worker3()
-stop = time.time()
-print(f"Worker3 processtime: {stop - start}")
