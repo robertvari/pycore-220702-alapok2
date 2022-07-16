748998c5499205a3207ba3dc424a2748cd706dca
Robert Vári <mail.robertvari@gmail.com>
2022-07-16 16:22:15 +0200

    update

---
+++ 07_default_parameters.py
@@ -0,0 +1,10 @@
+def registration_form(email, password1, password2):
+    print("Thank you for your registration.")
+    print(f"We sent an email to {email} with further instructions.")
+
+
+registration_form(
+    email="robert@gmail.com",
+    password1="testpas123",
+    password2="testpas123",
+)
