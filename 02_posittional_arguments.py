cdec90194907aa01ae1f8259abe648d8e7dc63a0
Robert Vári <mail.robertvari@gmail.com>
2022-07-16 14:50:20 +0200

    update

--- 02_posittional_arguments.py
+++ 02_posittional_arguments.py
@@ -9,9 +9,24 @@ def say_hello2(name, phone, address):
     print(f"Hello {name}")
     print(f"Your phone number is: {phone}")
     print(f"Your address is: {address}")
+# say_hello2("Robert", "06 20 555 6677", "Pécs")
 
 
-say_hello2("Robert", "06 20 555 6677", "Pécs")
+# parameter notation: name: str, fave_numbers: lst, age: int
+def say_hello3(name: str, phone: str, age: int, address: str):
+    print(f"Hello {name}")
+    print(f"Your phone number is: {phone}")
+    print(f"Your address is: {address}")
+    print(f"Your age: {age}")
 
-# string name = "Csaba"
-# int age = 30
+
+# say_hello3("Robert", "06 20 555 6677", "Pécs", 30)
+
+
+# call function with keyword parameters
+say_hello3(
+    age=40,
+    address="Pécs",
+    name="Kriszta",
+    phone="06 20 777 8899"
+)
