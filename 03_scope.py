5f97af8bf264c74433653a1316cee2380a713e6b
Robert Vári <mail.robertvari@gmail.com>
2022-07-16 15:05:30 +0200

    update

---
+++ 03_scope.py
@@ -0,0 +1,15 @@
+PHOTO_FOLDER_PATH = "C:\\photos"
+
+
+def say_hello():
+    print(PHOTO_FOLDER_PATH)
+
+
+def say_hello2():
+    print(PHOTO_FOLDER_PATH)
+    name = "Kriszta"
+    print(name)
+
+
+say_hello()
+say_hello2()
