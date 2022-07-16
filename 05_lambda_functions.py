3dd70f08654596e98d8a22661002153a77c9ed74
Robert Vári <mail.robertvari@gmail.com>
2022-07-16 15:44:49 +0200

    update

--- 05_lambda_functions.py
+++ 05_lambda_functions.py
@@ -4,13 +4,17 @@ divide_numbers = lambda a, b: a/b
 
 
 name_list = ["Vári Róbert", "Kiss Elemér", "Nagy Adrienn", "Tóth Barna", "Kiss Elemér Aladár"]
-print(sorted(name_list))
-print(sorted(name_list, key=lambda name: name.split()[-1]))
+# print(sorted(name_list))
+# print(sorted(name_list, key=lambda name: name.split()[-1]))
 
 
-student_list = {
+movie_list = {
     1: {"name": "The Princess", "rating": 63},
     2: {"name": "Spider-Man: No Way Home", "rating": 81},
     3: {"name": "Morbius", "rating": 64},
     4: {"name": "The Northman", "rating": 72},
 }
+
+result = sorted(movie_list, reverse=True, key=lambda movie_id: movie_list[movie_id]["rating"])
+for i in result:
+    print(f"Title: {movie_list[i]['name']} Rating: {movie_list[i]['rating']}")
