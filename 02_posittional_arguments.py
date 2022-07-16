be5cdf497bce79576d38d648023ea708f103c573
Robert Vári <mail.robertvari@gmail.com>
2022-07-16 14:36:12 +0200

    init

---
+++ 02_posittional_arguments.py
@@ -0,0 +1,14 @@
+# required positional argument
+def say_hello(name):
+    print(f"Hello {name}")
+# say_hello("Kata")
+
+
+# required positional argument
+def say_hello2(name, phone, address):
+    print(f"Hello {name}")
+    print(f"Your phone number is: {phone}")
+    print(f"Your address is: {address}")
+
+
+say_hello2("Robert", "06 20 555 6677", "Pécs")
