e04861d86c34ed275ac40801f3f23b2bcdaf9783
Robert Vári <mail.robertvari@gmail.com>
2022-07-17 15:41:50 +0200

    update

---
+++ TRexRunner/utilities/resources.py
@@ -0,0 +1,11 @@
+import os
+
+PROJECT_ROOT = os.path.dirname(__file__).replace("utilities", "")
+RESOURCES_PATH = os.path.join(PROJECT_ROOT, "resources")
+
+
+def get_resource_path(resource_name):
+    full_path = os.path.join(RESOURCES_PATH, resource_name)
+    assert os.path.exists(full_path), f"File does not exist: {full_path}"
+
+    return full_path
