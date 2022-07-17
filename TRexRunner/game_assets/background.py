a7bd599071d491ab6205ae8d58eb57ea6cadda66
Robert Vári <mail.robertvari@gmail.com>
2022-07-17 16:57:15 +0200

    update

--- TRexRunner/game_assets/background.py
+++ TRexRunner/game_assets/background.py
@@ -1,11 +1,18 @@
 import pygame
 from utilities.resources import get_resource_path
+from utilities.settings import *
 
 MOUNTAIN_IMAGE = pygame.image.load(get_resource_path("mountains.png")).convert()
+
 CLOUDS_IMAGE = pygame.image.load(get_resource_path("clouds.png")).convert_alpha()
+CLOUD_X_POS = 0
 
 
 def draw_background(screen):
+    global CLOUD_X_POS
+
     screen.blit(MOUNTAIN_IMAGE, (-300, -80))
-    screen.blit(CLOUDS_IMAGE, (0, 0))
+
+    CLOUD_X_POS -= GROUND_SPEED / 8
+    screen.blit(CLOUDS_IMAGE, (CLOUD_X_POS, 0))
 
