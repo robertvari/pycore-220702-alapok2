0747c66a3957dc24338718475fc6ca1285d50aa6
Robert Vári <mail.robertvari@gmail.com>
2022-07-17 17:32:52 +0200

    update

--- TRexRunner/game_assets/background.py
+++ TRexRunner/game_assets/background.py
@@ -5,14 +5,24 @@ from utilities.settings import *
 MOUNTAIN_IMAGE = pygame.image.load(get_resource_path("mountains.png")).convert()
 
 CLOUDS_IMAGE = pygame.image.load(get_resource_path("clouds.png")).convert_alpha()
-CLOUD_X_POS = 0
+CLOUDS_IMAGE_1_RECT = CLOUDS_IMAGE.get_rect(x=0, y=0)
+CLOUDS_IMAGE_2_RECT = CLOUDS_IMAGE.get_rect(topleft=CLOUDS_IMAGE_1_RECT.topright)
 
 
 def draw_background(screen):
-    global CLOUD_X_POS
-
     screen.blit(MOUNTAIN_IMAGE, (-300, -80))
 
-    CLOUD_X_POS -= GROUND_SPEED / 8
-    screen.blit(CLOUDS_IMAGE, (CLOUD_X_POS, 0))
+    if CLOUDS_IMAGE_1_RECT.right < 0:
+        CLOUDS_IMAGE_1_RECT.topleft = CLOUDS_IMAGE_2_RECT.topright
 
+    if CLOUDS_IMAGE_2_RECT.right < 0:
+        CLOUDS_IMAGE_2_RECT.topleft = CLOUDS_IMAGE_1_RECT.topright
+
+    screen.blit(CLOUDS_IMAGE, CLOUDS_IMAGE_1_RECT)
+
+    CLOUDS_IMAGE_1_RECT.x -= GROUND_SPEED // 6
+    # pygame.draw.rect(screen, "blue", CLOUDS_IMAGE_1_RECT, 10)
+
+    CLOUDS_IMAGE_2_RECT.x -= GROUND_SPEED // 6
+    screen.blit(CLOUDS_IMAGE, CLOUDS_IMAGE_2_RECT)
+    # pygame.draw.rect(screen, "red", CLOUDS_IMAGE_2_RECT, 10)
