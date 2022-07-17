76680bb81a812e717295b1f8064a090dc3505fcb
Robert Vári <mail.robertvari@gmail.com>
2022-07-17 15:29:29 +0200

    update

--- TRexRunner/game_assets/background.py
+++ TRexRunner/game_assets/background.py
@@ -5,5 +5,4 @@ CLOUDS_IMAGE = pygame.image.load("resources/clouds.png")
 
 
 def draw_background(screen):
-    pass
-    # print("Drawing background...")
+    screen.blit(MOUNTAIN_IMAGE, (-300, -80))
